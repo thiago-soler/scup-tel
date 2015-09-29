d718044cd70a47ac5a0432c6045f7e4f27f29681
thiago-soler <solerzao@gmail.com>
2015-09-29 00:52:18 -0300

    Creating repository.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scupTelApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the scupTelApp
+ */
+angular.module('scupTelApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scupTelApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the scupTelApp
+ */
+angular.module('scupTelApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
